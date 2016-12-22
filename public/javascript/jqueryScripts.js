fde85175d718bada201e3156bd0598833da6656e
Justin Mills <jbmills@gmail.com>
2016-12-22 09:47:56 -0600

    Almost completed all menus on index

--- public/javascript/jqueryScripts.js
+++ public/javascript/jqueryScripts.js
@@ -5,7 +5,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 
 function openNav() {
-    document.getElementById("mySidenav").style.height = "50vh";
+    document.getElementById("mySidenav").style.height = "90vh";
     // document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
@@ -13,9 +13,61 @@ function openNav() {
 function openNestedSeating() {
     document.getElementById("myNestedSeating").style.height = "100vh";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function openNestedLighting() {
+    document.getElementById("myNestedLighting").style.height = "50vh";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 
 }
 
+function openNestedStorage() {
+    document.getElementById("myNestedStorage").style.height = "50vh";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+}
+
+function openNestedTables() {
+    document.getElementById("myNestedTables").style.height = "100vh";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+}
+
+function openNestedMirrors() {
+    document.getElementById("myNestedMirrors").style.height = "30vh";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+}
+
+function openNestedFolk() {
+    document.getElementById("myNestedFolk").style.height = "30vh";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+}
+
+function openNestedRugs() {
+    document.getElementById("myNestedRugs").style.height = "30vh";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+}
+
+function openNestedServeware() {
+    document.getElementById("myNestedServeware").style.height = "30vh";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+}
+
+function openNestedWallDec() {
+    document.getElementById("myNestedWallDec").style.height = "30vh";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+}
+
+function openNestedGarden() {
+    document.getElementById("myNestedGarden").style.height = "30vh";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+}
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 
 function closeNav() {
@@ -26,5 +78,50 @@ function closeNav() {
 
 function closeNestedSeating() {
     document.getElementById("myNestedSeating").style.height = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function closeNestedLighting() {
+    document.getElementById("myNestedLighting").style.height = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function closeNestedStorage() {
+    document.getElementById("myNestedStorage").style.height = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function closeNestedTables() {
+    document.getElementById("myNestedTables").style.height = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function closeNestedMirrors() {
+    document.getElementById("myNestedMirrors").style.height = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function closeNestedFolk() {
+    document.getElementById("myNestedFolk").style.height = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function closeNestedRugs() {
+    document.getElementById("myNestedRugs").style.height = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function closeNestedServeware() {
+    document.getElementById("myNestedServeware").style.height = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function closeNestedWallDec() {
+    document.getElementById("myNestedWallDec").style.height = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function closeNestedGarden() {
+    document.getElementById("myNestedGarden").style.height = "0";
     document.body.style.backgroundColor = "white";
 }
