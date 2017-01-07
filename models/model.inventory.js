6bfad98d4451362a0182e037d4c2b8a8bf296add
Justin Mills <jbmills@gmail.com>
2017-01-06 22:43:42 -0800

    adjusting inventory

--- models/model.inventory.js
+++ models/model.inventory.js
@@ -1,12 +1,52 @@
 var mongoose = require("mongoose");
 
 InventorySchema = new mongoose.Schema({
-    description: String,
-    reference: String,
-    height: Number,
-    width: Number,
-    depth: Number,
-    price: Number
+    // images: Array,
+    // name: String,
+    // period: String,
+    // description: String,
+    // reference: String,
+    // condition: String,
+    // quantity: Number,
+    // height: String,
+    // width: String,
+    // depth: String,
+    // diameter: String,
+    // price: Number,
+    // location: {
+    //     type: String,
+    //     default: "886 N Palm Canyon Dr. Palm Springs, CA 92262 US"
+    // },
+    // available: {
+    //     type: Boolean,
+    //     default: true
+    // }
+
+    "name": String,
+    "period": String,
+    "description": String,
+    "price": String,
+    "condition": String,
+    "measurements": {
+        "width": {
+            type: String,
+            default: "N/A"
+        },
+        "height": {
+            type: String,
+            default: "N/A"
+        },
+        "depth": {
+            type: String,
+            default: "N/A"
+        },
+        "diameter": {
+            type: String,
+            default: "N/A"
+        },
+    },
+    "numOfItems": String,
+    "imageUrl": String
 
 });
 
