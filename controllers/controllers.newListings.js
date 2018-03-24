518d47659158af680d3d0cabb33137af93602d0d
Justin Mills <jbmills@gmail.com>
2018-03-24 13:50:12 -0700

    made some changes to backend

--- controllers/controllers.newListings.js
+++ controllers/controllers.newListings.js
@@ -7,7 +7,7 @@ var LIST = require("../models/model.newListings.js"),
     auth: {
       type: "login",
       user: "alamodincps@gmail.com",
-      pass: "Mrjack1965$"
+      pass: "Mrboo1977$"
     }
   });
 
