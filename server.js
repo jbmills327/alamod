fdcda0a666b495d844b56da60f322d0ddb2bc764
Justin Mills <jbmills@gmail.com>
2025-02-22 16:03:06 -0800

    Updated Server js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require("express"),
   mongoose = require("mongoose"),
   routes = require("./routes"),
   https = require("https"),
+  http = require("http"),
   fs = require("fs");
 
 // SSL certificate paths
@@ -12,36 +13,42 @@ const options = {
   cert: fs.readFileSync("/etc/letsencrypt/live/alamodps.com/fullchain.pem"),
 };
 
-// test
-// var PORT = process.env.PORT || 3000;
-
-// production
-var PORT = process.env.PORT || 443;
+var HTTPS_PORT = process.env.PORT || 443;
+var HTTP_PORT = 80; // Standard HTTP port
 
 var app = express();
 
+// Redirect all HTTP requests to HTTPS
+http.createServer((req, res) => {
+  res.writeHead(301, { "Location": "https://www.alamodps.com" + req.url });
+  res.end();
+}).listen(HTTP_PORT, () => {
+  console.log(`HTTP Server is running on port ${HTTP_PORT}, redirecting to HTTPS`);
+});
+
+// Connect to MongoDB
 mongoose.connect("mongodb://localhost/alamod");
 
 app.use(express.static("public"));
-
-// mount our morgan logger middleware
 app.use(logger);
-
-// use body-parser to parse the body of our POST requests
-app.post('*', bodyParser.urlencoded({
-  extended: true
-}));
-
-// also parse the json data in the request
+app.post('*', bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Force www on HTTPS requests
+app.use((req, res, next) => {
+  if (!req.hostname.startsWith("www.")) {
+    return res.redirect(301, `https://www.alamodps.com${req.url}`);
+  }
+  next();
+});
 
 routes(app);
 
-// create the app listener using HTTPS
-https.createServer(options, app).listen(PORT, (err) => {
+// Start HTTPS server
+https.createServer(options, app).listen(HTTPS_PORT, (err) => {
   if (err) {
     console.log("Server error: ", err);
     process.exit(1);
   }
-  console.log("HTTPS Server is up on port", PORT);
+  console.log("HTTPS Server is up on port", HTTPS_PORT);
 });
